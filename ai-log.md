e8fa47149b4c553025b310cef371dae357f24c9e
Fabian144 <fabianyousif24@gmail.com>
2025-12-15 18:51:53 +0100

    Update AI log and try adding fetched data to new variable

--- ai-log.md
+++ ai-log.md
@@ -5,9 +5,9 @@ Jag använde ChatGPT och lite av DuckDuckGos inbyggda AI för att lära mig viss
 **Delarna av koden som jag lärde mig skriva med hjälp av AI:**
 - Fetch genom try, catch och await i en async funktion
 - Att man kan använda det fetchade datat (t.ex. i en ny funktion) med .then efter att ha kallat på async funktionen
-- Mappa objekt för att modifiera, lägga till eller ta bort nycklar eller värden
+- Mappa arrayer för att modifiera, lägga till eller ta bort nycklar eller värden
 
 **Implementeringen och värdering:**
 
-ChatGPT föreslog att jag använder map() för objekten ur fetchade datat för att kunna spara dem i en variabel och använda datat utifrån den. Det var för att jag ville ha värdet för tiden då programmet startar som ett faktiskt datum (new Date()) istället för en sträng, då kan man enkelt modifiera varje "start" nyckel på det sättet.
+ChatGPT föreslog att jag använder map() för arrayen med fetchade datat för att kunna spara dem i en variabel och använda datat utifrån den. Det var för att jag ville ha värdet för tiden då programmet startar som ett faktiskt datum (new Date()) istället för en sträng, då kan man enkelt modifiera varje "start" nyckel på det sättet.
 Då undrade jag hur man gör om man vill ha med alla nyckelvärdepar, i koden väljer jag bara ut två av dem (rad 114-117), för att det tredje ändå inte behöver visas upp. Det jag blev osäker på var om det här var bästa sättet att göra det på om man har många fler nyckelvärdepar som man ska använda. Då läste jag på och lärde mig om spread operator, som används om man vill använda en array utan att behöva skriva in själva arrayen, utan man skriver namnet (variabeln) på arrayen med tre punkter framför. På så sätt kan man konstatera att man vill ha med alla nyckelvärdepar när man använder map() men kan lägga till, ta bort eller modifiera specifika nyckelvärdepar på samma gång.
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //--------LÅT STÅ-----------
 // Definiera fasta "konstanter" för olika typer av animering av menyn
 ANIMATION = {
-  NONE: "none", // Ingen animation
-  TIMER: "timer", // setInterval-baserad animation
-  ALTERNATIVE: "alternative", // ytterligare alternativ
+  NONE: 'none', // Ingen animation
+  TIMER: 'timer', // setInterval-baserad animation
+  ALTERNATIVE: 'alternative', // ytterligare alternativ
 };
 
 // Ändra värdet för att styra vilken meny-animation som ska användas
@@ -13,51 +13,51 @@ window.MENU_ANIMATION_MODE ??= ANIMATION.ALTERNATIVE; // ANIMATION.TIMER (Defaul
  Användningsexempel för animationer beroende på inställning
 */
 if (window.MENU_ANIMATION_MODE === ANIMATION.NONE) {
-  console.log("Ingen meny-animation används");
+  console.log('Ingen meny-animation används');
 
   function toggleMenu() {
-    const menu = document.querySelector("ul.menu");
-    const menuIcon = document.querySelector(".menu-icon > .fas");
+    const menu = document.querySelector('ul.menu');
+    const menuIcon = document.querySelector('.menu-icon > .fas');
 
-    if (!menu.classList.contains("menu--show")) {
-      menu.classList.add("menu--show");
-      menuIcon.classList.add("fa-times");
+    if (!menu.classList.contains('menu--show')) {
+      menu.classList.add('menu--show');
+      menuIcon.classList.add('fa-times');
     } else {
-      menu.classList.remove("menu--show");
-      menuIcon.classList.remove("fa-times");
+      menu.classList.remove('menu--show');
+      menuIcon.classList.remove('fa-times');
     }
   }
 } else if (window.MENU_ANIMATION_MODE === ANIMATION.TIMER) {
-  console.log("Meny-animation med timer används");
+  console.log('Meny-animation med timer används');
 
   const originalMenuPosition = parseFloat(
-    window.getComputedStyle(document.querySelector("ul.menu")).left
+    window.getComputedStyle(document.querySelector('ul.menu')).left
   ); // parseFloat returnerar alla float värden som är före en annan värdetyp ur en sträng, i det här fallet -300 ur "-300px"
 
   function toggleMenu() {
     let menuPositionLeft = parseFloat(
-      window.getComputedStyle(document.querySelector("ul.menu")).left
+      window.getComputedStyle(document.querySelector('ul.menu')).left
     );
-    const menu = document.querySelector("ul.menu");
+    const menu = document.querySelector('ul.menu');
 
     if (menuPositionLeft < 0) {
-      document.querySelector(".menu-icon > .fas").classList.add("fa-times");
+      document.querySelector('.menu-icon > .fas').classList.add('fa-times');
 
       const menuIntervalAnimationIn = setInterval(() => {
         if (menuPositionLeft < 0) {
           menuPositionLeft += 20;
-          menu.style.left = menuPositionLeft + "px";
+          menu.style.left = menuPositionLeft + 'px';
         } else {
           clearInterval(menuIntervalAnimationIn);
         }
       }, 0);
     } else {
-      document.querySelector(".menu-icon > .fas").classList.remove("fa-times");
+      document.querySelector('.menu-icon > .fas').classList.remove('fa-times');
 
       const menuIntervalAnimationOut = setInterval(() => {
         if (menuPositionLeft > originalMenuPosition) {
           menuPositionLeft -= 20;
-          menu.style.left = menuPositionLeft + "px";
+          menu.style.left = menuPositionLeft + 'px';
         } else {
           clearInterval(menuIntervalAnimationOut);
         }
@@ -65,28 +65,115 @@ if (window.MENU_ANIMATION_MODE === ANIMATION.NONE) {
     }
   }
 } else if (window.MENU_ANIMATION_MODE === ANIMATION.ALTERNATIVE) {
-  console.log("Meny-animation med alternativ metod används");
+  console.log('Meny-animation med alternativ metod används');
 
   function toggleMenu() {
-    const menu = document.querySelector("ul.menu");
-    const menuIcon = document.querySelector(".menu-icon > .fas");
+    const menu = document.querySelector('ul.menu');
+    const menuIcon = document.querySelector('.menu-icon > .fas');
 
-    menu.style.transition = "left, 500ms";
+    menu.style.transition = 'left, 500ms';
 
-    if (!menu.classList.contains("menu--show")) {
-      menu.classList.add("menu--show");
-      menuIcon.classList.add("fa-times");
+    if (!menu.classList.contains('menu--show')) {
+      menu.classList.add('menu--show');
+      menuIcon.classList.add('fa-times');
     } else {
-      menu.classList.remove("menu--show");
-      menuIcon.classList.remove("fa-times");
+      menu.classList.remove('menu--show');
+      menuIcon.classList.remove('fa-times');
     }
   }
 }
 //--------------------------
 
-async function fetchData(url) {
-  // Visar loading gif innan någon data laddats in
-  document.querySelector("#js-loading").classList.remove("hidden");
+createListGroupElement();
+setChannel('SVT 1');
+
+function createListGroupElement() { // Skapar ul elementet för programmen och lägger in "tidigare program" knappen
+  let listGroupElement = `<ul class="list-group list-group-flush">
+		<li class="list-group-item show-previous hidden">Visa tidigare program</li>
+	</ul>`;
+
+  document.querySelector('#js-schedule').innerHTML = listGroupElement;
+}
+
+function setChannel(channelName) {
+  document.querySelector('#js-title').innerText = channelName;
+
+  fetchData(`./data/${channelName}.json`).then(setData);
+  let dataFromFetch;
+
+  function setData(data) {
+    dataFromFetch = data;
+  }
+
+  console.log(dataFromFetch);
+
+  // Varje objekt från datat läggs in i variabeln med bara nyckelvärdeparen som ska användas i koden och sedan visas up
+  const programs = dataFromFetch.map((program) => ({
+    start: new Date(program.start),
+    name: program.name,
+  }));
+
+  // Sorterar programmen stigande efter datum
+  programs.sort((firstProgram, secondProgram) => {
+    return firstProgram.start - secondProgram.start;
+  });
+
+  // Funktion för programstarts tiden, så timmarna och minuterna visar tvåsiffriga tal även om de är under 10
+  function formatTime(hours, minutes) {
+    if (hours < 10) hours = '0' + hours;
+    if (minutes < 10) minutes = '0' + minutes;
+    return `${hours}:${minutes}`;
+  }
+
+  let listItems = ``;
+
+  //------ Går igenom alla program objekt och sparar dem i HTML format i övre variabeln
+  programs.forEach((program) => {
+    /* Elementen läggs till med en klass som gömmer dem om programstartens datum och tid är
+			mindre än (alltså före) ett simulerat datum */
+    if (program.start < new Date('2021-02-10T19:00:00+01:00')) {
+      listItems += `<li class="list-group-item hidden">
+					<strong>${formatTime(program.start.getHours(), program.start.getMinutes())}</strong>
+					<div>${program.name}</div>
+				</li>`;
+    } else {
+      listItems += `<li class="list-group-item">
+				<strong>${formatTime(program.start.getHours(), program.start.getMinutes())}</strong>
+				<div>${program.name}</div>
+			</li>`;
+    }
+  });
+  //------
+
+  // Sparade program objekten i HTML format läggs in i ett existerande HTML element via variabeln
+  document.querySelector('.list-group').innerHTML += listItems;
+
+  // Gömmer loading gif när datan både laddats in och visas upp på sidan
+  document.querySelector('#js-loading').classList.add('hidden');
+
+  //------ Knappen "Visa tidigare program" visas och fungerar bara om minst ett av programmen är gömda, alltså redan sända
+  let firstProgram = document.querySelectorAll('.list-group-item')[1];
+
+  if (firstProgram.classList.contains('hidden')) {
+    document.querySelector('.show-previous').classList.remove('hidden');
+  }
+
+  document.querySelector('.show-previous').addEventListener('click', showPrevious);
+  //------
+
+  function showPrevious() {
+    let listItems = document.querySelectorAll('.list-group-item');
+
+    listItems.forEach((listItem) => {
+      listItem.classList.remove('hidden');
+    });
+
+    document.querySelector('.show-previous').classList.add('hidden');
+  }
+}
+
+async function fetchData(url) { // Visar loading gif innan någon data laddats in
+  document.querySelector('#js-loading').classList.remove('hidden');
 
   try {
     const response = await fetch(url);
@@ -94,88 +181,6 @@ async function fetchData(url) {
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("Fetch failed:", error);
+    console.error('Fetch failed:', error);
   }
 }
-
-function setChannel(channelName) {
-  document.querySelector("#js-title").innerText = channelName;
-
-  //------ Skapar ul elementet för programmen och lägger in "tidigare program" knappen
-  let list = `<ul class="list-group list-group-flush">
-		<li class="list-group-item show-previous hidden">Visa tidigare program</li>
-	</ul>`;
-	
-  document.querySelector("#js-schedule").innerHTML = list;
-	//------
-
-  fetchData(`./data/${channelName}.json`).then(useData);
-
-  function useData(dataFromFetch) {
-    // Varje objekt från datat läggs in i variabeln med bara nyckelvärdeparen som ska användas i koden och sedan visas up
-    const programs = dataFromFetch.map((program) => ({
-      start: new Date(program.start),
-      name: program.name,
-    }));
-
-    // Sorterar programmen stigande efter datum
-    programs.sort((firstProgram, secondProgram) => {
-      return firstProgram.start - secondProgram.start;
-    });
-
-    // Funktion för programstarts tiden, så timmarna och minuterna visar tvåsiffriga tal även om de är under 10
-    function formatTime(hours, minutes) {
-      if (hours < 10) hours = "0" + hours;
-      if (minutes < 10) minutes = "0" + minutes;
-      return `${hours}:${minutes}`;
-    }
-
-    let listItems = ``;
-
-    //------ Går igenom alla program objekt och sparar dem i HTML format i övre variabeln
-    programs.forEach((program) => {
-      /* Elementen läggs till med en klass som gömmer dem om programstartens datum och tid är
-			mindre än (alltså före) ett simulerat datum */
-      if (program.start < new Date("2021-02-10T19:00:00+01:00")) {
-        listItems += `<li class="list-group-item hidden">
-					<strong>${formatTime(program.start.getHours(), program.start.getMinutes())}</strong>
-					<div>${program.name}</div>
-				</li>`;
-      } else {
-        listItems += `<li class="list-group-item">
-				<strong>${formatTime(program.start.getHours(), program.start.getMinutes())}</strong>
-				<div>${program.name}</div>
-			</li>`;
-      }
-    });
-		//------
-
-    // Sparade program objekten i HTML format läggs in i ett existerande HTML element via variabeln
-    document.querySelector(".list-group").innerHTML += listItems;
-
-    // Gömmer loading gif när datan både laddats in och visas upp på sidan
-    document.querySelector("#js-loading").classList.add("hidden");
-
-    //------ Knappen "Visa tidigare program" visas och fungerar bara om minst ett av programmen är gömda, alltså redan sända
-    let firstProgram = document.querySelectorAll(".list-group-item")[1];
-
-    if (firstProgram.classList.contains("hidden")) {
-      document.querySelector(".show-previous").classList.remove("hidden");
-    }
-
-    document.querySelector(".show-previous").addEventListener("click", showPrevious);
-		//------
-  }
-
-  function showPrevious() {
-    let listItems = document.querySelectorAll(".list-group-item");
-
-    listItems.forEach((listItem) => {
-      listItem.classList.remove("hidden");
-    });
-
-    document.querySelector(".show-previous").classList.add("hidden");
-  }
-}
-
-setChannel("SVT 1");
